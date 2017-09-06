ad66648e4dcfeb5283f69c60aebe5373c34ba699
Steven Meyer <steven@stdlib.com>
2017-09-06 15:37:11 -0700

    Removed callbacks from new tests

--- tests/files/cases/function_valid_default_int_and_number_params.js
+++ tests/files/cases/function_valid_default_int_and_number_params.js
@@ -4,9 +4,5 @@
 * @param {number} b beta
 * @return {string}
 */
-module.exports = function(a = 12, b = 342, context, callback) {
-  // valid - this test is in response to issue #1 on github https://github.com/faaslang/faaslang/issues/1
-
-  callback(null, `OK`);
-
-};
+module.exports = function(a = 12, b = 342, context, callback) {};
+// valid - this test is in response to issue #1 on github https://github.com/faaslang/faaslang/issues/1
