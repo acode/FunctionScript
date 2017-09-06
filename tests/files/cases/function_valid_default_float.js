9183d5056b63a9e39467daff26438a0ce63762eb
Steven Meyer <steven@stdlib.com>
2017-09-06 15:42:04 -0700

    Removed return value from new tests

--- tests/files/cases/function_valid_default_float.js
+++ tests/files/cases/function_valid_default_float.js
@@ -1,7 +1,6 @@
 /**
 * My function
 * @param {float} a alpha
-* @return {string}
 */
 module.exports = function(a = 3.14, context, callback) {};
 // valid - this test is in response to issue #1 on github https://github.com/faaslang/faaslang/issues/1
