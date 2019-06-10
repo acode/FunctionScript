af36b0d0f133c32c9f997bbc3cafc84d942bb194
Keith Horwood <keithwhor@gmail.com>
2019-06-10 13:39:23 -0700

    Add support for HTTP Daemon

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   FunctionParser: require('./lib/parser/function_parser.js'),
+  Daemon: require('./lib/daemon.js'),
   Gateway: require('./lib/gateway.js'),
   types: require('./lib/types.js')
 };
