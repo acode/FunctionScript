8385dfb5be1036d56b152f4e86de5039c1cf73b5
Jacob <jacoblee93@gmail.com>
2019-08-16 13:52:10 -0700

    Adds proxy parameter support, validations for acceptable parameter names

---
+++ tests/files/cases/function_invalid_parameter_name.js
@@ -0,0 +1,9 @@
+/**
+* Invalid function with an invalid parameter name charge
+* @returns {string}
+*/
+module.exports = ($badparam, callback) => {
+
+  return callback(null, 'invalid parameter name');
+
+};
---
+++ tests/files/cases/function_valid_parameter_with_underscores.js
@@ -0,0 +1,9 @@
+/**
+* Invalid function with an invalid parameter name charge
+* @returns {string}
+*/
+module.exports = (good_param_, __good_param, callback) => {
+
+  return callback(null, 'valid parameter name');
+
+};
