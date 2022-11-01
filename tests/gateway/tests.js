2e9c1b558dec89702078b7a14523e6d754cd45d3
Jacob Lee <jacob@autocode.com>
2022-11-01 14:31:50 -0700

    Adds SubscriptionUpgradeRequiredError error type

--- tests/gateway/tests.js
+++ tests/gateway/tests.js
@@ -1281,6 +1281,29 @@ module.exports = (expect) => {
 
   });
 
+  it('Should register an error in the resolve step with type SubscriptionUpgradeRequiredError', done => {
+
+    let originalResolveFn = FaaSGateway.resolve;
+    FaaSGateway.resolve = (req, res, buffer, callback) => {
+      let error = new Error('You are not allowed to access this API.');
+      error.subscriptionUpgradeRequiredError = true;
+      return callback(error);
+    };
+
+    request('POST', {}, '/my_function/', {}, (err, res, result) => {
+
+      FaaSGateway.resolve = originalResolveFn;
+
+      expect(err).to.not.exist;
+      expect(res.statusCode).to.equal(401);
+      expect(result.error).to.exist;
+      expect(result.error.type).to.equal('SubscriptionUpgradeRequiredError');
+      done();
+
+    });
+
+  });
+
   it('Should register an error in the resolve step with type PaymentRequiredError', done => {
 
     let originalResolveFn = FaaSGateway.resolve;
