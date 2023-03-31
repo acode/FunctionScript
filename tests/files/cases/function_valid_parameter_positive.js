68b3b7e292e0fb4f0cfcab738b3ee529772bdd6e
Keith Horwood <keithwhor@gmail.com>
2023-03-31 16:49:05 -0700

    Support negatives in default params

---
+++ tests/files/cases/function_valid_parameter_positive.js
@@ -0,0 +1,9 @@
+/**
+* My function
+* @param {string} a alpha
+* @param {number} b beta
+* @returns {string}
+*/
+module.exports = async (a = 'hi', b = +7, context) => {
+  // valid, negative number
+};
